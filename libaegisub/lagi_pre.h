9087c2d28d09cfbc8d29ab96af42efc3a1b641a1
Amar Takhar <verm@aegisub.org>
2010-03-22 12:23:55 +0000

    Populate lagi_pre.h
    git-svn-id: svn+ssh://aegisub/var/svn/aegisub/branches/libaegisub@4180 93549f3f-7f0a-0410-a4b3-e966c9c94f04

--- libaegisub/lagi_pre.h
+++ libaegisub/lagi_pre.h
@@ -19,3 +19,24 @@
 #include <wx/colour.h>
 
 #include "aegisub/exception.h"
+#define LAGI_PRE
+
+#include <errno.h>
+#include <math.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+
+#include "libaegisub/cajun/elements.h"
+#include "libaegisub/cajun/reader.h"
+#include "libaegisub/cajun/visitor.h"
+#include "libaegisub/cajun/writer.h"
+
+#include <wx/colour.h>
+
+#include "aegisub/exception.h"
