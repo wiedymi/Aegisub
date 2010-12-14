a3c0d8805ca14b40b1a15eb81d3b6f8a028b1232
Amar Takhar <verm@aegisub.org>
2010-12-14 05:38:24 +0000

    1/3 done video/ command conversion.
    git-svn-id: svn+ssh://aegisub/var/svn/aegisub/branches/new_menu_hotkey@5000 93549f3f-7f0a-0410-a4b3-e966c9c94f04

--- src/command/video.cpp
+++ src/command/video.cpp
@@ -7,13 +7,13 @@
 // modification, are permitted provided that the following conditions are met:
 //
 //   * Redistributions of source code must retain the above copyright notice,
-//     this list of conditions and the following disclaimer.
+//	 this list of conditions and the following disclaimer.
 //   * Redistributions in binary form must reproduce the above copyright notice,
-//     this list of conditions and the following disclaimer in the documentation
-//     and/or other materials provided with the distribution.
+//	 this list of conditions and the following disclaimer in the documentation
+//	 and/or other materials provided with the distribution.
 //   * Neither the name of the Aegisub Group nor the names of its contributors
-//     may be used to endorse or promote products derived from this software
-//     without specific prior written permission.
+//	 may be used to endorse or promote products derived from this software
+//	 without specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -41,6 +41,8 @@
 #ifndef AGI_PRE
 #endif
 
+#include "command.h"
+
 #include "aegisub/context.h"
 #include "video_context.h"
 #include "main.h"
@@ -56,205 +58,345 @@
 
 namespace cmd {
 
-void video_aspect_cinematic(agi::Context *c) {
-    VideoContext::Get()->Stop();
-    VideoContext::Get()->SetAspectRatio(3);
-    wxGetApp().frame->SetDisplayMode(1,-1);
-}
+
+class video_aspect_cinematic: public Command {
+public:
+	CMD_NAME("video/aspect/cinematic")
+	STR_MENU("&Cinematic (2.35)")
+	STR_DISP("Cinematic (235)")
+	STR_HELP("Forces video to 2.35 aspect ratio.")
+
+	void operator()(agi::Context *c) {
+	VideoContext::Get()->Stop();
+	VideoContext::Get()->SetAspectRatio(3);
+	wxGetApp().frame->SetDisplayMode(1,-1);
+	}
+};
 
 
-void video_aspect_custom(agi::Context *c) {
-    VideoContext::Get()->Stop();
+class video_aspect_custom: public Command {
+public:
+	CMD_NAME("video/aspect/custom")
+	STR_MENU("Custom..")
+	STR_DISP("Custom")
+	STR_HELP("Forces video to a custom aspect ratio.")
 
-    wxString value = wxGetTextFromUser(_("Enter aspect ratio in either:\n  decimal (e.g. 2.35)\n  fractional (e.g. 16:9)\n  specific resolution (e.g. 853x480)"),_("Enter aspect ratio"),AegiFloatToString(VideoContext::Get()->GetAspectRatioValue()));
-    if (value.IsEmpty()) return;
+	void operator()(agi::Context *c) {
+		VideoContext::Get()->Stop();
 
-    value.MakeLower();
+		wxString value = wxGetTextFromUser(_("Enter aspect ratio in either:\n  decimal (e.g. 2.35)\n  fractional (e.g. 16:9)\n  specific resolution (e.g. 853x480)"),_("Enter aspect ratio"),AegiFloatToString(VideoContext::Get()->GetAspectRatioValue()));
+		if (value.IsEmpty()) return;
 
-    // Process text
-    double numval;
-    if (value.ToDouble(&numval)) {
-        //Nothing to see here, move along
-    }
-    else {
-        double a,b;
-        int pos=0;
-        bool scale=false;
+		value.MakeLower();
 
-        //Why bloat using Contains when we can just check the output of Find?
-        pos = value.Find(':');
-        if (pos==wxNOT_FOUND) pos = value.Find('/');
-        if (pos==wxNOT_FOUND&&value.Contains(_T('x'))) {
-            pos = value.Find('x');
-            scale=true;
-        }
+		// Process text
+		double numval;
+		if (value.ToDouble(&numval)) {
+			//Nothing to see here, move along
+		}
+		else {
+			double a,b;
+			int pos=0;
+			bool scale=false;
 
-        if (pos>0) {
-            wxString num = value.Left(pos);
-            wxString denum = value.Mid(pos+1);
-            if (num.ToDouble(&a) && denum.ToDouble(&b) && b!=0) {
-                numval = a/b;
-                if (scale) c->videoBox->videoDisplay->SetZoom(b / VideoContext::Get()->GetHeight());
-            }
-        }
-        else numval = 0.0;
-    }
+			//Why bloat using Contains when we can just check the output of Find?
+			pos = value.Find(':');
+			if (pos==wxNOT_FOUND) pos = value.Find('/');
+			if (pos==wxNOT_FOUND&&value.Contains(_T('x'))) {
+				pos = value.Find('x');
+				scale=true;
+			}
 
-    // Sanity check
-    if (numval < 0.5 || numval > 5.0) wxMessageBox(_("Invalid value! Aspect ratio must be between 0.5 and 5.0."),_("Invalid Aspect Ratio"),wxICON_ERROR|wxOK);
+			if (pos>0) {
+				wxString num = value.Left(pos);
+				wxString denum = value.Mid(pos+1);
+				if (num.ToDouble(&a) && denum.ToDouble(&b) && b!=0) {
+					numval = a/b;
+					if (scale) c->videoBox->videoDisplay->SetZoom(b / VideoContext::Get()->GetHeight());
+				}
+			}
+			else numval = 0.0;
+		}
 
-    // Set value
-    else {
-        VideoContext::Get()->SetAspectRatio(4,numval);
-        wxGetApp().frame->SetDisplayMode(1,-1);
-    }
-}
+		// Sanity check
+		if (numval < 0.5 || numval > 5.0) wxMessageBox(_("Invalid value! Aspect ratio must be between 0.5 and 5.0."),_("Invalid Aspect Ratio"),wxICON_ERROR|wxOK);
 
-
-void video_aspect_default(agi::Context *c) {
-    VideoContext::Get()->Stop();
-    VideoContext::Get()->SetAspectRatio(0);
-    wxGetApp().frame->SetDisplayMode(1,-1);
-}
-
-
-void video_aspect_full(agi::Context *c) {
-    VideoContext::Get()->Stop();
-    VideoContext::Get()->SetAspectRatio(1);
-    wxGetApp().frame->SetDisplayMode(1,-1);
-}
+		// Set value
+		else {
+			VideoContext::Get()->SetAspectRatio(4,numval);
+			wxGetApp().frame->SetDisplayMode(1,-1);
+		}
+	}
+};
 
 
-void video_aspect_wide(agi::Context *c) {
-    VideoContext::Get()->Stop();
-    VideoContext::Get()->SetAspectRatio(2);
-    wxGetApp().frame->SetDisplayMode(1,-1);
-}
+
+class video_aspect_default: public Command {
+public:
+	CMD_NAME("video/aspect/default")
+	STR_MENU("&Default")
+	STR_DISP("Default")
+	STR_HELP("Leave video on original aspect ratio.")
+
+	void operator()(agi::Context *c) {
+		VideoContext::Get()->Stop();
+		VideoContext::Get()->SetAspectRatio(0);
+		wxGetApp().frame->SetDisplayMode(1,-1);
+	}
+};
 
 
-void video_close(agi::Context *c) {
-	wxGetApp().frame->LoadVideo(_T(""));
-}
+
+class video_aspect_full: public Command {
+public:
+	CMD_NAME("video/aspect/full")
+	STR_MENU("&Fullscreen (4:3)")
+	STR_DISP("Fullscreen (4:3)")
+	STR_HELP("Forces video to 4:3 aspect ratio.")
+
+	void operator()(agi::Context *c) {
+		VideoContext::Get()->Stop();
+		VideoContext::Get()->SetAspectRatio(1);
+		wxGetApp().frame->SetDisplayMode(1,-1);
+	}
+};
 
 
-void video_detach(agi::Context *c) {
-	wxGetApp().frame->DetachVideo(!c->detachedVideo);
-}
+class video_aspect_wide: public Command {
+public:
+	CMD_NAME("video/aspect/wide")
+	STR_MENU("&Widescreen (16:9)")
+	STR_DISP("Widescreen (16:9)")
+	STR_HELP("Forces video to 16:9 aspect ratio.")
+
+	void operator()(agi::Context *c) {
+		VideoContext::Get()->Stop();
+		VideoContext::Get()->SetAspectRatio(2);
+		wxGetApp().frame->SetDisplayMode(1,-1);
+	}
+};
 
 
-void video_details(agi::Context *c) {
-    VideoContext::Get()->Stop();
-    DialogVideoDetails videodetails(c->parent);
-    videodetails.ShowModal();
-}
+class video_close: public Command {
+public:
+	CMD_NAME("video/close")
+	STR_MENU("&Close Video")
+	STR_DISP("Close Video")
+	STR_HELP("Closes the currently open video file.")
+
+	void operator()(agi::Context *c) {
+		wxGetApp().frame->LoadVideo(_T(""));
+	}
+};
+
+
+
+class video_detach: public Command {
+public:
+	CMD_NAME("video/detach")
+	STR_MENU("Detach Video")
+	STR_DISP("Detach Video")
+	STR_HELP("Detach video, displaying it in a separate Window.")
+
+	void operator()(agi::Context *c) {
+		wxGetApp().frame->DetachVideo(!c->detachedVideo);
+	}
+};
+
+
+class video_details: public Command {
+public:
+	CMD_NAME("video/details")
+	STR_MENU("Show Video Details..")
+	STR_DISP("Show Video Details")
+	STR_HELP("Shows video details.")
+
+	void operator()(agi::Context *c) {
+		VideoContext::Get()->Stop();
+		DialogVideoDetails videodetails(c->parent);
+		videodetails.ShowModal();
+	}
+};
 
 
 void video_focus_seek(agi::Context *c) {
-    wxWindow *curFocus = wxWindow::FindFocus();
-    if (curFocus == c->videoBox->videoSlider) {
-        if (c->PreviousFocus) c->PreviousFocus->SetFocus();
-    }
-    else {
-        c->PreviousFocus = curFocus;
-        c->videoBox->videoSlider->SetFocus();
-    }
+	wxWindow *curFocus = wxWindow::FindFocus();
+	if (curFocus == c->videoBox->videoSlider) {
+		if (c->PreviousFocus) c->PreviousFocus->SetFocus();
+	}
+	else {
+		c->PreviousFocus = curFocus;
+		c->videoBox->videoSlider->SetFocus();
+	}
 }
 
 
 void video_frame_next(agi::Context *c) {
-    c->videoBox->videoSlider->NextFrame();
+	c->videoBox->videoSlider->NextFrame();
 }
 
 
 void video_frame_play(agi::Context *c) {
-    VideoContext::Get()->Play();
+	VideoContext::Get()->Play();
 }
 
 
 void video_frame_prev(agi::Context *c) {
-    c->videoBox->videoSlider->PrevFrame();
+	c->videoBox->videoSlider->PrevFrame();
 }
 
 
-void video_jump(agi::Context *c) {
-    VideoContext::Get()->Stop();
-    if (VideoContext::Get()->IsLoaded()) {
-        DialogJumpTo JumpTo(c->parent);
-        JumpTo.ShowModal();
-        c->videoBox->videoSlider->SetFocus();
-    }
-}
+
+class video_jump: public Command {
+public:
+	CMD_NAME("video/jump")
+	STR_MENU("&Jump to..")
+	STR_DISP("Jump to")
+	STR_HELP("Jump to frame or time.")
+
+	void operator()(agi::Context *c) {
+		VideoContext::Get()->Stop();
+		if (VideoContext::Get()->IsLoaded()) {
+			DialogJumpTo JumpTo(c->parent);
+			JumpTo.ShowModal();
+			c->videoBox->videoSlider->SetFocus();
+		}
+	}
+};
 
 
-void video_jump_end(agi::Context *c) {
-    c->SubsGrid->SetVideoToSubs(false);
-}
+class video_jump_end: public Command {
+public:
+	CMD_NAME("video/jump/end")
+	STR_MENU("Jump Video to End")
+	STR_DISP("Jump Video to End")
+	STR_HELP("Jumps the video to the end frame of current subtitle.")
+
+	void operator()(agi::Context *c) {
+		c->SubsGrid->SetVideoToSubs(false);
+	}
+};
 
 
-void video_jump_start(agi::Context *c) {
-    c->SubsGrid->SetVideoToSubs(true);
-}
+class video_jump_start: public Command {
+public:
+	CMD_NAME("video/jump/start")
+	STR_MENU("Jump Video to Start")
+	STR_DISP("Jump Video to Start")
+	STR_HELP("Jumps the video to the start frame of current subtitle.")
+
+	void operator()(agi::Context *c) {
+		c->SubsGrid->SetVideoToSubs(true);
+	}
+};
 
 
-void video_open(agi::Context *c) {
-    wxString path = lagi_wxString(OPT_GET("Path/Last/Video")->GetString());
-    wxString str = wxString(_("Video Formats")) + _T(" (*.avi,*.mkv,*.mp4,*.avs,*.d2v,*.ogm,*.mpeg,*.mpg,*.vob,*.mov)|*.avi;*.avs;*.d2v;*.mkv;*.ogm;*.mp4;*.mpeg;*.mpg;*.vob;*.mov|")
-                 + _("All Files") + _T(" (*.*)|*.*");
-    wxString filename = wxFileSelector(_("Open video file"),path,_T(""),_T(""),str,wxFD_OPEN | wxFD_FILE_MUST_EXIST);
-    if (!filename.empty()) {
-        wxGetApp().frame->LoadVideo(filename);
-        OPT_SET("Path/Last/Video")->SetString(STD_STR(filename));
-    }
-}
+class video_open: public Command {
+public:
+	CMD_NAME("video/open")
+	STR_MENU("&Open Video..")
+	STR_DISP("Open Video")
+	STR_HELP("Opens a video file.")
+
+	void operator()(agi::Context *c) {
+		wxString path = lagi_wxString(OPT_GET("Path/Last/Video")->GetString());
+		wxString str = wxString(_("Video Formats")) + _T(" (*.avi,*.mkv,*.mp4,*.avs,*.d2v,*.ogm,*.mpeg,*.mpg,*.vob,*.mov)|*.avi;*.avs;*.d2v;*.mkv;*.ogm;*.mp4;*.mpeg;*.mpg;*.vob;*.mov|")
+					 + _("All Files") + _T(" (*.*)|*.*");
+		wxString filename = wxFileSelector(_("Open video file"),path,_T(""),_T(""),str,wxFD_OPEN | wxFD_FILE_MUST_EXIST);
+		if (!filename.empty()) {
+			wxGetApp().frame->LoadVideo(filename);
+			OPT_SET("Path/Last/Video")->SetString(STD_STR(filename));
+		}
+	}
+};
 
 
-void video_open_dummy(agi::Context *c) {
-    wxString fn;
-    if (DialogDummyVideo::CreateDummyVideo(c->parent, fn)) {
-        wxGetApp().frame->LoadVideo(fn);
-    }
-}
+class video_open_dummy: public Command {
+public:
+	CMD_NAME("video/open/dummy")
+	STR_MENU("Use Dummy Video..")
+	STR_DISP("Use Dummy Video")
+	STR_HELP("Opens a video clip with solid colour.")
+
+	void operator()(agi::Context *c) {
+		wxString fn;
+		if (DialogDummyVideo::CreateDummyVideo(c->parent, fn)) {
+			wxGetApp().frame->LoadVideo(fn);
+		}
+	}
+};
 
 
-void video_show_overscan(agi::Context *c) {
+class video_show_overscan: public Command {
+public:
+	CMD_NAME("video/show_overscan")
+	STR_MENU("Show Overscan Mask")
+	STR_DISP("Show Overscan Mask")
+	STR_HELP("Show a mask over the video, indicating areas that might get cropped off by overscan on televisions.")
+
+	void operator()(agi::Context *c) {
 //XXX: Fix to not require using an event. (maybe)
-//    OPT_SET("Video/Overscan Mask")->SetBool(event.IsChecked());
-    VideoContext::Get()->Stop();
-    c->videoBox->videoDisplay->Render();
-}
+//		OPT_SET("Video/Overscan Mask")->SetBool(event.IsChecked());
+		VideoContext::Get()->Stop();
+		c->videoBox->videoDisplay->Render();
+	}
+};
 
 
-void video_zoom_100(agi::Context *c) {
-    VideoContext::Get()->Stop();
-    c->videoBox->videoDisplay->SetZoom(1.);
+class video_zoom_100: public Command {
+public:
+	CMD_NAME("video/zoom/100")
+	STR_MENU("&100%")
+	STR_DISP("100%")
+	STR_HELP("Set zoom to 100%.")
 
-}
+	void operator()(agi::Context *c) {
+		VideoContext::Get()->Stop();
+		c->videoBox->videoDisplay->SetZoom(1.);
+	}
+};
 
 
-void video_zoom_200(agi::Context *c) {
-    VideoContext::Get()->Stop();
-    c->videoBox->videoDisplay->SetZoom(2.);
 
-}
+class video_zoom_200: public Command {
+public:
+	CMD_NAME("video/zoom/200")
+	STR_MENU("&200%")
+	STR_DISP("200%")
+	STR_HELP("Set zoom to 200%.")
+
+	void operator()(agi::Context *c) {
+		VideoContext::Get()->Stop();
+		c->videoBox->videoDisplay->SetZoom(2.);
+	}
+};
 
 
-void video_zoom_50(agi::Context *c) {
-    VideoContext::Get()->Stop();
-    c->videoBox->videoDisplay->SetZoom(.5);
+class video_zoom_50: public Command {
+public:
+	CMD_NAME("video/zoom/50")
+	STR_MENU("&50%")
+	STR_DISP("50%")
+	STR_HELP("Set zoom to 50%.")
 
-}
+	void operator()(agi::Context *c) {
+		VideoContext::Get()->Stop();
+		c->videoBox->videoDisplay->SetZoom(.5);
+	}
+};
+
 
 
 void video_zoom_in(agi::Context *c) {
-    VideoContext::Get()->Stop();
-    c->videoBox->videoDisplay->SetZoom(c->videoBox->videoDisplay->GetZoom() + .125);
+	VideoContext::Get()->Stop();
+	c->videoBox->videoDisplay->SetZoom(c->videoBox->videoDisplay->GetZoom() + .125);
 
 }
 
 
 void video_zoom_out(agi::Context *c) {
-    VideoContext::Get()->Stop();
-    c->videoBox->videoDisplay->SetZoom(c->videoBox->videoDisplay->GetZoom() - .125);
+	VideoContext::Get()->Stop();
+	c->videoBox->videoDisplay->SetZoom(c->videoBox->videoDisplay->GetZoom() - .125);
 
 }
 
